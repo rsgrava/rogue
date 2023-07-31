65c041e705efafaeca9a64513bb5ab84b10d09cf
rsgrava <rsgrava@hotmail.com>
2023-07-31 01:36:53 -0300

    main: draw using push

--- main.lua
+++ main.lua
@@ -1,7 +1,12 @@
+push = require("libs/push")
 Gamestate = require("libs/gamestate")
+require("src/constants")
 require("src/states/main_menu")
 
 function love.load()
+    local window_w, window_h = love.window.getDesktopDimensions()
+    push:setupScreen(GAME_W, GAME_H, window_w, window_h,
+                     {fullscreen = true, resizable = true, vsync = true})
     Gamestate.switch(mainMenuState)
 end
 
@@ -10,8 +15,11 @@ function love.update()
 end
 
 function love.draw()
-    Gamestate.current():draw(dt)
+    push:start()
+        Gamestate.current():draw(dt)
+    push:finish()
 end
 
 function love.resize(w, h)
+    push:resize(w, h)
 end
