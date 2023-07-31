4c64228d40c6c6ad98b1501389248a10b5b0561c
rsgrava <rsgrava@hotmail.com>
2023-07-31 01:30:03 -0300

    main: use gamestate lib in load/update/draw

--- main.lua
+++ main.lua
@@ -1,10 +1,16 @@
+Gamestate = require("libs/gamestate")
+require("src/states/main_menu")
+
 function love.load()
+    Gamestate.switch(mainMenuState)
 end
 
 function love.update()
+    Gamestate.current():update(dt)
 end
 
 function love.draw()
+    Gamestate.current():draw(dt)
 end
 
 function love.resize(w, h)
