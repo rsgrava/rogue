3f9ce7c27fc0fcbd32c40b5a30a17f5c19323843
rsgrava <rsgrava@hotmail.com>
2023-08-01 23:49:46 -0300

    tile: implement visibility

--- src/tile.lua
+++ src/tile.lua
@@ -5,16 +5,28 @@ Tile = Class{}
 
 function Tile:init(id)
     self.tileDef = db.tiles[id]
+    self.visible = false
+    self.wasExplored = false
 end
 
 function Tile:draw(x, y)
-    love.graphics.draw(
-        self.tileDef.texture,
-        love.graphics.newQuad(self.tileDef.tileX * TILE_W, self.tileDef.tileY * TILE_H,
-            TILE_W, TILE_H, self.tileDef.texture),
-        x,
-        y
-    )
+    if self.wasExplored then
+        if not self.visible then
+            love.graphics.setColor(0.5, 0.5, 0.5)
+        end
+        love.graphics.draw(
+            self.tileDef.texture,
+            love.graphics.newQuad(self.tileDef.tileX * TILE_W, self.tileDef.tileY * TILE_H,
+                TILE_W, TILE_H, self.tileDef.texture),
+            x,
+            y
+        )
+        love.graphics.setColor(1, 1, 1)
+    else
+        love.graphics.setColor(0, 0, 0)
+        love.graphics.rectangle("fill", x, y, TILE_W - 1, TILE_H - 1)
+        love.graphics.setColor(1, 1, 1)
+    end
 end
 
 function Tile:canWalk()
