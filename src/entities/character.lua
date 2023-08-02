be4b703c8bcbc66f35c2dca2f73500f4c0e713ad
rsgrava <rsgrava@hotmail.com>
2023-08-02 19:48:11 -0300

    entities: character: implement movement

--- src/entities/character.lua
+++ src/entities/character.lua
@@ -1,19 +1,56 @@
 Class = require("libs/class")
+require("src/utils")
 require("src/entities/sprite")
 
 Character = Class{}
 
 function Character:init(defs)
+    local def = db.characters[defs.id]
+    self.sprite = Sprite({
+        texture1 = def.texture1,
+        texture2 = def.texture2,
+        quadX = def.quadX,
+        quadY = def.quadY
+    })
     self.tileX = defs.tileX
     self.tileY = defs.tileY
-    self.sprite = Sprite({
-        texture1 = defs.texture1,
-        texture2 = defs.texture2,
-        quadX = defs.quadX,
-        quadY = defs.quadY
-    })
+    self.blocks = true
 end
 
-function Character:draw()
-    self.sprite:draw(self.tileX * TILE_W, self.tileY * TILE_H)
+function Character:draw(map)
+    if map:isVisible(self.tileX, self.tileY) then
+        self.sprite:draw(self.tileX * TILE_W, self.tileY * TILE_H)
+    end
+end
+
+function Character:tryMove(map, objects, dx, dy)
+    if not map:isBlocked(objects, self.tileX + dx, self.tileY + dy) then
+        self.tileX = self.tileX + dx
+        self.tileY = self.tileY + dy
+        return true
+    end
+    return false
+end
+
+
+function Character:moveTowards(map, objects, x, y)
+    local dx = x - self.tileX
+    local dy = y - self.tileY
+    local distance = math.sqrt(dx^2 + dy^2)
+    
+    dx = math.round(dx / distance)
+    dy = math.round(dy / distance)
+
+    self:tryMove(map, objects, dx, dy)
+end
+
+function Character:moveAwayFrom(map, objects, x, y)
+    local dx = self.tileX - x
+    local dy = self.tileY - y
+    local distance = math.sqrt(dx^2 + dy^2)
+    
+    dx = math.round(dx / distance)
+    dy = math.round(dy / distance)
+
+    self:tryMove(map, objects, dx, dy)
 end
