d8fcca9b004d9e15438f97de72fc4b2752cbbe21
rsgrava <rsgrava@hotmail.com>
2023-08-02 16:13:53 -0300

    entities: sprite: rename drawable to sprite

---
+++ src/entities/sprite.lua
@@ -0,0 +1,25 @@
+require("src/global_animation")
+
+Sprite = Class{}
+
+function Sprite:init(texture1, texture2, tileX, tileY)
+    self.x = 0
+    self.y = 0
+    self.texture1 = texture1
+    self.texture2 = texture2
+    self.quad = love.graphics.newQuad(tileX * TILE_W, tileY * TILE_H, TILE_W, TILE_H, texture1)
+end
+
+function Sprite:draw()
+    local texture = self.texture1
+    if GlobalAnimation.frame == 2 and self.texture2 ~= nil then
+        texture = self.texture2
+    end
+
+    love.graphics.draw(
+        texture,
+        self.quad,
+        self.x,
+        self.y
+    )
+end
