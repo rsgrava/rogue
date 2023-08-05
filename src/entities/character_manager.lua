59a340e3438854f24f5179e64665d611735a93a0
rsgrava <rsgrava@hotmail.com>
2023-08-05 13:06:39 -0300

    entities: character_manager: implement character manager

---
+++ src/entities/character_manager.lua
@@ -0,0 +1,64 @@
+Class = require("libs/class")
+require("src/utils")
+
+CharacterManager = Class{}
+
+function CharacterManager:init()
+    self.characters = {}
+end
+
+function CharacterManager:draw()
+    for characterId, character in pairs(self.characters) do
+        character:draw()
+    end
+end
+
+function CharacterManager:insert(item)
+    table.insert(self.characters, item)
+end
+
+function CharacterManager:insertList(list)
+    for characterId, character in pairs(list) do
+        self:insert(character)
+    end
+end
+
+function CharacterManager:remove(item)
+    for characterId, character in pairs(self.characters) do
+        if item == character then
+            table.remove(self.characters, characterId)
+            break
+        end
+    end
+end
+
+function CharacterManager:clearDead()
+    for characterId, character in pairs(self.characters) do
+        if character.dead then
+            table.remove(self.characters, characterId)
+        end
+    end
+end
+
+function CharacterManager:getAt(x, y)
+    for characterId, character in pairs(self.characters) do
+        if character.tileX == x and character.tileY == y then
+            return character
+        end
+    end
+end
+
+function CharacterManager:isBlocked(x, y)
+    for characterId, character in pairs(self.characters) do
+        if character.tileX == x and character.tileY == y and character.blocks then
+            return character
+        end
+    end
+end
+
+-- TODO: replace with scheduler
+function CharacterManager:takeTurns()
+    for characterId, character in pairs(self.characters) do
+        character:takeTurn()
+    end
+end
