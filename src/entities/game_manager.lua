f2e56b8780fc1e326620fee1ab9a2778684a92e6
rsgrava <rsgrava@hotmail.com>
2023-08-05 13:07:12 -0300

    entities: game_manager: implement game manager

---
+++ src/entities/game_manager.lua
@@ -0,0 +1,33 @@
+require("src/entities/character_manager")
+require("src/entities/object_manager")
+
+Game = {}
+
+function Game.init()
+    local characters, startX, startY
+    Game.map, characters, startX, startY = generateDungeon({
+        width = 100,
+        height = 100,
+        minRooms = 20,
+        maxRooms = 30,
+        minSize = 6,
+        maxSize = 10
+    })
+
+    Game.objects = ObjectManager()
+    Game.characters = CharacterManager()
+    Game.characters:insertList(characters)
+
+    Game.player = PC({
+        id = "player",
+        tileX = startX,
+        tileY = startY
+    })
+end
+
+function Game.isBlocked(x, y)
+    if not Game.map:canWalk(x, y) then
+        return true
+    end
+    return Game.characters:isBlocked(x, y)
+end
