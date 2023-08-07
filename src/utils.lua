2346cc0fef3c8f61a12840924c7432dd8e397730
rsgrava <rsgrava@hotmail.com>
2023-08-07 15:28:08 -0300

    utils: implement inverse cantor function

--- src/utils.lua
+++ src/utils.lua
@@ -19,3 +19,10 @@ function cantor(x, y)
     return (x + y) * (x + y + 1) / 2 + y
 end
 
+function inverseCantor(z)
+    local w = math.floor((math.sqrt(8 * z + 1) - 1) / 2)
+    local t = (w^2 + w) / 2
+    local y = z - t
+    local x = w - y
+    return x, y
+end
